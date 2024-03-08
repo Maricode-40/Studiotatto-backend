282ba8d20a3978695e6f593500460706a4952132
Isabel Rico <marissa.rico28@gmail.com>
2024-03-08 11:36:20 +0100

    adding an objet in constant folder with the three properties or values from my roles

---
+++ src/constants/UserRoles.ts
@@ -0,0 +1,7 @@
+import { Role } from "../models/Role";
+
+export const UserRoles = {
+  SUPERADMIN: { name: "superadmin" },
+  TATUADOR: { name: "tatuador" },
+  USER: { name: "user" } as Role,
+};
--- src/database/seeders/RoleSeeder.ts
+++ src/database/seeders/RoleSeeder.ts
@@ -1,12 +1,13 @@
+import { UserRoles } from "../../constants/UserRoles";
 import { Role } from "../../models/Role";
 import { Seeder } from "./Seeder";
 
 export class RoleSeeder extends Seeder {
   protected async generate(): Promise<void> {
     const roles: Partial<Role>[] = [
-      { id: 1, name: "superadmin" },
-      { id: 2, name: "tatuador" },
-      { id: 3, name: "user" },
+      UserRoles.SUPERADMIN,
+      UserRoles.TATUADOR,
+      UserRoles.USER,
     ];
 
     await Role.save(roles);
